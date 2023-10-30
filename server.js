b5881e1198da0cc8f19c6b73eb55f7097108e96f
Jonaira Manha <abcd@Jonaeath.local>
2023-10-30 14:27:12 +0600

    last commit

--- server.js
+++ server.js
@@ -37,6 +37,34 @@ app.post('/users',(req, res)=>{
     })
 })
 
+app.get('/read/:id',async(req,res)=>{
+    const sql = "SELECT * FROM users WHERE id = ?";
+    const id = req.params.id;
+    db.query(sql,[id], (err,data)=>{
+        if(err) return res.json(err);
+        return res.json(data);
+    })
+})
+
+
+app.put('/update/:id',(req,res)=>{
+    const sql = 'UPDATE users SET `username`=?, `email`=? WHERE id=?';
+    const id = req.params.id;
+    db.query(sql, [req.body.username, req.body.email, id], (err, result)=>{
+        if(err) return res.json({message:"Inside Server Error"});
+        return res.json(result);
+    })
+})
+
+app.delete('/delete/:id',(req,res)=>{
+    const sql = "DELETE FROM users WHERE id=?";
+    const id = req.params.id;
+    db.query(sql, [id], (err, result)=>{
+        if(err) return res.json({message:"Inside Server Error"});
+        return res.json(result);
+    })
+})
+
 
 app.get("/", async(req, res) => {
     res.send("Test server is running");
